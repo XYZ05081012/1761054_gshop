9ccd4f00ad759e5d7f7519588948af70ea539309
陈彦 <chenyan@chenyandeMacBook-Air.local>
2019-03-29 14:49:18 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-if="$route.path==='/msite' || $route.path==='/order' || $route.path==='/search' || $route.path==='/profile' "/>
+  </div>
+</template>
+
+<script>
+
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+  export default {
+
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
